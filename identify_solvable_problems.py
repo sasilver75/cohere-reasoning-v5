f366770a2415cbd40fb9049eb31eedad68c5a177
Sam Silver <sasilver0051@Gmail.com>
2024-11-20 23:45:33 -0800

    10 interesting problems

--- identify_solvable_problems.py
+++ identify_solvable_problems.py
@@ -15,12 +15,12 @@ from the cohere-reasoning-v4 project.
 HELPER = CohereExperimentHelper()  # Encapsulates logic about the specific models we're using
 SOURCE_PATH = Path("datasets/original/cn_k12_math_problems.csv")
 SINK_PATH = Path("datasets/derived/interesting_problems.csv")
-SEED = 42
-TARGET_N_SOLVABLE_PROBLEMS = 3  # The number of solvable problems we want to identify
-N_SOLUTIONS_PER_PROBLEM = 3  # For each problem, the number of solution attempts over which we'll evaluate problem difficulty
-LOWER_SUCCESS_RATE_BOUND = 0.0  # The lower bound on the success rate of the solutions we'll accept as solvable/interesting; Number if [0, 1)
-UPPER_SUCCESS_RATE_BOUND = .8  # The upper bound on the success rate of the solutions we'll accept as solvable/interesting; Number in [0, 1)
+TARGET_N_SOLVABLE_PROBLEMS = 10  # The number of solvable problems we want to identify
+N_SOLUTION_ATTEMPTS_PER_PROBLEM = 4  # For each problem, the number of solution attempts over which we'll evaluate problem difficulty. Note that without retries we'll have 2*{N_SOLUTION_ATTEMPTS_PER_PROBLEM} API calls per problem.
+LOWER_SUCCESS_RATE_BOUND = 0.2  # The lower bound on the success rate of the solutions we'll accept as solvable/interesting; Number if [0, 1)
+UPPER_SUCCESS_RATE_BOUND = .6  # The upper bound on the success rate of the solutions we'll accept as solvable/interesting; Number in [0, 1)
 EPSILON = 1e-10  # To help with floating point division giving .199999 when it really should be .2
+SEED = 42  # Random seed for dataset shuffling; We'll iterate through rows of this shuffled dataset until we identify the target number of solvable problems.
 # END OF TUNABLE PARAMETERS
 # PARAMETER CHECKS (Do not change)
 if not (0 <= LOWER_SUCCESS_RATE_BOUND < UPPER_SUCCESS_RATE_BOUND < 1):
@@ -53,10 +53,10 @@ async def _appraise_problem(row: pd.Series) -> tuple[bool, list[pd.Series]]:
     If the problem is solvable, return a list of the incorrect solutions so that they can be re-used down the pipeline as 
     on-policy incorrect solutions.
     """
-    # Generate N_SOLUTIONS_PER_PROBLEM solutions and collect the incorrect ones
+    # Generate N_SOLUTION_ATTEMPTS_PER_PROBLEM solutions and collect the incorrect ones
     solution_attempts = [
         _generate_and_verify_solution(row)
-        for _ in range(N_SOLUTIONS_PER_PROBLEM)
+        for _ in range(N_SOLUTION_ATTEMPTS_PER_PROBLEM)
     ]
     results = await asyncio.gather(*solution_attempts)
     incorrect_solutions: list[pd.Series] = [attempt_data for attempt_success, attempt_data in results if not attempt_success]
@@ -66,7 +66,7 @@ async def _appraise_problem(row: pd.Series) -> tuple[bool, list[pd.Series]]:
         augmented_row["solution_id"] = idx
 
     # Did we find the appropriate number of incorrect solutions?
-    failure_rate = len(incorrect_solutions) / N_SOLUTIONS_PER_PROBLEM
+    failure_rate = len(incorrect_solutions) / N_SOLUTION_ATTEMPTS_PER_PROBLEM
     success_rate = 1 - failure_rate
 
     print(f"Success rate for row {row["row_id"]}: {success_rate}")
@@ -94,22 +94,11 @@ async def main():
     # ~~~ (2) Appraise Problems ~~~
     # For each problem, appraise it. If it's solvable, add the incorrect solutions to the accumulator.
     # Evaluate problems until we've found the target number of solvable problems.
-
-    # async def _process_problem(row: pd.Series):
-    #     nonlocal n_solvable_problems, incorrect_solutions
-    #     if n_solvable_problems >= TARGET_N_SOLVABLE_PROBLEMS:
-    #         return
-
-    #     is_solvable, incorrect_solution_rows = await _appraise_problem(row)
-    #     if is_solvable:
-    #         n_solvable_problems += 1
-    #         incorrect_solutions.extend(incorrect_solution_rows)
-    #         print(f"Found {n_solvable_problems}/{TARGET_N_SOLVABLE_PROBLEMS} solvable problems.")
-    # tasks = [process_problem(row) for _, row in shuffled_df.iterrows()]
-    # await atqdm.gather(*tasks)  # Populates `incorrect_solutions` list
-
     print(f"Appraising problems until we've found {TARGET_N_SOLVABLE_PROBLEMS} solvable problems...")
+    
     # Let's choose to do this sequentially for now, until we have enough solutions.
+    # TODO: This is too slow. We need to parallelize this, but also not make too many calls needlessly. 
+    # For small N, we don't want to exhaust our bucket for no reason ($); we can use semaphores, or batching, or...
     pbar = tqdm(total=TARGET_N_SOLVABLE_PROBLEMS, colour="green", desc="Finding solvable problems")
     for _, row in shuffled_df.iterrows():
         is_solvable, incorrect_solution_rows = await _appraise_problem(row)
---
+++ pad_remaining_incorrect_solutions_on_policy.py
@@ -0,0 +1,6 @@
+
+"""
+This is similar to the generate_prefixes_remaining_on_policy.py script from v4, but uses a Token Bucket strategy instead,
+as well as the Helper classes from v5.
+"""
+
